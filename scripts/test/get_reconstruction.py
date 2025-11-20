d4676e49b078620ce513d85cd9509b0202e41bb8
ericx1107 <xushiyu1107@gmail.com>
2025-11-20 09:25:36 +0000

    add das calculations in recon test file

--- scripts/test/get_reconstruction.py
+++ scripts/test/get_reconstruction.py
@@ -35,6 +35,7 @@ from keypoint_diffuser.datasets.shapespartial import ShapesPartial
 from keypoint_diffuser.options.ae_options import AEConfig, AEOptions
 from keypoint_diffuser.utils.eval_metrics import EMD_CD_recon
 from keypoint_diffuser.utils.pc_utils import collate_fn
+from keypoint_diffuser.utils.utils import normalize_to_box_multi
 from keypoint_diffuser.utils.transforms import (
     ApplyToBoth,
     Collect,
@@ -52,50 +53,13 @@ PARTIAL_VIEW_MODES = ["default", "myopia", "patch", "tac"]
 # ----------------------------
 # CLI
 # ----------------------------
-def save_pred_kp_geoms(
-    predicted, names, opt, out_dir: Path = Path("geom_np"), vis_max=1000
-):
-    """
-    Saves raw numpy arrays for each sample:
-      - pred_kp.npy   : predicted keypoints (normalized)
-    Also dumps meta.json with ids + config.
-    """
-
-    out_dir = out_dir / opt.model / opt.category
-    out_dir.mkdir(parents=True, exist_ok=True)
-
-    saved = 0
-    for idx, (kpcd_norm, name) in enumerate(zip(predicted, names)):
-        if saved >= vis_max:
-            break
-
-        pred = np.asarray(kpcd_norm, dtype=np.float32)  # normalized preds
-
-        # save per-sample folder
-        dst = out_dir / name
-        dst.mkdir(parents=True, exist_ok=True)
-        np.save(dst / "pred_kp.npy", pred)
-
-        meta = {
-            "model": opt.model,
-            "category": opt.category,
-            "model_id": name,
-            "num_pred": int(pred.shape[0]),
-        }
-        with open(dst / "kpmeta.json", "w") as f:
-            json.dump(meta, f, indent=2)
-
-        saved += 1
-
-    print(f"[✓] Saved {saved} samples as numpy arrays under {out_dir}/")
-
 
 def save_recon_geoms(
-    best_recons, best_gts, best_inputs, best_names, cds, emds, opt, out_dir: Path = Path("output")
+    recons, gts, inputs, names, partial_sample_names, kps, cds, emds, opt, out_dir: Path = Path("output")
 ):
     """
-    Save best reconstructions and ground truths as numpy arrays.
-      - recon.npy : best reconstruction (normalized point cloud)
+    Save reconstructions and ground truths as numpy arrays.
+      - recon.npy : reconstruction (normalized point cloud)
       - gt.npy    : ground truth point cloud
       - meta.json : per-sample metadata (with metrics)
     """
@@ -104,7 +68,7 @@ def save_recon_geoms(
     out_dir.mkdir(parents=True, exist_ok=True)
 
     saved = 0
-    for (recon, gt, inpc, name, cd, emd) in zip(best_recons, best_gts, best_inputs, best_names, cds, emds):
+    for (recon, gt, inpc, name, patsam, kp, cd, emd) in zip(recons, gts, inputs, names, partial_sample_names, kps, cds, emds):
         # recon: [M, 3], gt: [N, 3]
         dst = out_dir / name
         dst.mkdir(parents=True, exist_ok=True)
@@ -112,6 +76,7 @@ def save_recon_geoms(
         np.save(dst / "recon.npy", np.asarray(recon, dtype=np.float32))
         np.save(dst / "gt.npy", np.asarray(gt, dtype=np.float32))
         np.save(dst / "input.npy", np.asarray(inpc, dtype=np.float32))
+        np.save(dst / "pred_kp.npy", np.asarray(kp, dtype=np.float32))
 
         meta = {
             "model": opt.model,
@@ -121,6 +86,7 @@ def save_recon_geoms(
             "num_gt": int(gt.shape[0]),
             "cd": float(cd),
             "emd": float(emd),
+            "partial_sample_name": patsam
         }
         with open(dst / "meta.json", "w") as f:
             json.dump(meta, f, indent=2)
@@ -147,16 +113,154 @@ def build_argparser() -> argparse.ArgumentParser:
         model_cls.get_parser(sp)
         try_add_arg(sp, "--ckpt", type=Path)
         try_add_arg(sp, "--category", type=str, default="chair")
-        try_add_arg(sp, "--batch-size", type=int, default=16)       # on default this is causing batch size to be 48 for some reason
+        try_add_arg(sp, "--batch-size", type=int, default=16)
         try_add_arg(sp, "--num-workers", type=int, default=8)
         try_add_arg(sp, "--db-path", type=Path, default=Path("results.db"))
         try_add_arg(sp, "--key-points", type=int, default=10)
         try_add_arg(sp, "--output-dir", type=Path, default=Path("recons_out"))
         try_add_arg(sp, "--input-type", type=str, default="full", choices=["full", *PARTIAL_VIEW_MODES, "all"])
-        try_add_arg(sp, "--save-kps", action="store_true")
 
     return p
 
+
+# ----------------------------
+# KP metrics utils
+# ----------------------------
+
+def fwd_alignment_scores(kpn_ds, predicted):
+    preds = []
+    for entry, kpcd, nfact in zip(kpn_ds, predicted["kpcd"], predicted["nfact"]):
+        dmax, dmin = nfact
+
+        ground_truths = []
+        for kp in entry["keypoints"]:
+            nkp = (kp["xyz"] - dmin) / (dmax - dmin)
+            nkp = 2.0 * (nkp - 0.5)
+
+            ground_truths.append(nkp)
+        ground_truths = np.array(ground_truths)
+        dist = np.sum(
+            (np.expand_dims(kpcd, 1) - np.expand_dims(ground_truths, 0)) ** 2, axis=-1
+        )
+        argminfwd = np.argmin(dist, -1)
+        preds.append([entry["keypoints"][argm]["semantic_id"] for argm in argminfwd])
+        
+    import pdb; pdb.set_trace()
+    acc = [np.mean(np.array(pa) == np.array(pb)) for pa in preds for pb in preds]
+
+    return np.mean(acc)
+
+
+def bwd_alignment_scores(kpn_ds, predicted):
+    preds = collections.defaultdict(list)
+    for entry, kpcd, nfact in zip(kpn_ds, predicted["kpcd"], predicted["nfact"]):
+        dmax, dmin = nfact
+        ground_truths = []
+        for kp in entry["keypoints"]:
+            nkp = (kp["xyz"] - dmin) / (dmax - dmin)
+            nkp = 2.0 * (nkp - 0.5)
+
+            ground_truths.append(nkp)
+        ground_truths = np.array(ground_truths)
+        dist = np.sum(
+            (np.expand_dims(kpcd, 1) - np.expand_dims(ground_truths, 0)) ** 2, axis=-1
+        )
+        argminbwd = np.argmin(dist, -2)
+        for i, kp in enumerate(entry["keypoints"]):
+            preds[kp["semantic_id"]].append(argminbwd[i])
+    q = []
+    for arr in preds.values():
+        arr = np.array(arr)
+        q.append(np.mean(arr[:, None] == arr[None, :]))
+    
+    return np.mean(q)
+
+def mIoU_thresh(kpn_ds, predicted, pcd_path, threshold=0.1):
+    """
+    calculates the mean Intersection over Union (mIoU) for the predicted keypoints against the ground truth keypoints.
+    predicted keypoints are projected back on to the original cloud surface before distance calculation, to measure geometric alignment.
+    """
+    kps = []
+    gts = []
+    for entry, kpcd, nfact in zip(kpn_ds, predicted["kpcd"], predicted["nfact"]):
+        cid = entry["class_id"]
+        mid = entry["model_id"]
+        pc = naive_read_pcd(pcd_path / cid / f"{mid}.pcd")
+        dmax, dmin = nfact
+
+        ground_truths = [pc[kp["pcd_info"]["point_index"]] for kp in entry["keypoints"]]
+        gts.append(ground_truths)
+
+        npc = (pc - dmin) / (dmax - dmin)
+        npc = 2.0 * (npc - 0.5)
+
+        dist = np.sqrt(
+            np.sum((np.expand_dims(kpcd, 1) - np.expand_dims(npc, 0)) ** 2, axis=-1)
+        )
+        kps.append(pc[np.argmin(dist, -1)])
+
+    npos = fp_sum = fn_sum = 0
+    for ground_truths, kpcd in zip(gts, kps):
+        dist = np.sqrt(
+            np.sum(
+                (np.expand_dims(kpcd, 1) - np.expand_dims(ground_truths, 0)) ** 2,
+                axis=-1,
+            )
+        )
+        npos += dist.shape[1]
+        fp_sum += np.sum(np.min(dist, -1) > threshold)
+        fn_sum += np.sum(np.min(dist, -2) > threshold)
+
+    return (npos - fn_sum) / (npos + fp_sum)
+
+def mIoU(kpn_ds, predicted, pcd_path):
+    """
+    calculates the mean Intersection over Union (mIoU) for the predicted keypoints against the ground truth keypoints.
+    predicted keypoints are projected back on to the original cloud surface before distance calculation, to measure geometric alignment.
+    """
+    thresholds = np.linspace(0.0, 0.1)
+    kps = []
+    gts = []
+    for entry, kpcd, nfact in zip(kpn_ds, predicted["kpcd"], predicted["nfact"]):
+        cid = entry["class_id"]
+        mid = entry["model_id"]
+        pc = naive_read_pcd(pcd_path / cid / f"{mid}.pcd")
+        dmax, dmin = nfact
+
+        ground_truths = [pc[kp["pcd_info"]["point_index"]] for kp in entry["keypoints"]]
+        gts.append(ground_truths)
+
+        npc = (pc - dmin) / (dmax - dmin)
+        npc = 2.0 * (npc - 0.5)
+
+        dist = np.sqrt(
+            np.sum((np.expand_dims(kpcd, 1) - np.expand_dims(npc, 0)) ** 2, axis=-1)
+        )
+        kps.append(pc[np.argmin(dist, -1)])
+    for threshold in thresholds:
+        npos = fp_sum = fn_sum = 0
+        for ground_truths, kpcd in zip(gts, kps):
+            dist = np.sqrt(
+                np.sum(
+                    (np.expand_dims(kpcd, 1) - np.expand_dims(ground_truths, 0)) ** 2,
+                    axis=-1,
+                )
+            )
+            npos += dist.shape[1]
+            fp_sum += np.sum(np.min(dist, -1) > threshold)
+            fn_sum += np.sum(np.min(dist, -2) > threshold)
+        yield (npos - fn_sum) / (npos + fp_sum)
+
+def mIoU_curve_plot(kpn_ds, predicted, pcd_path, out_root):
+    plt.style.use("seaborn")
+    miou_curve = list(mIoU(kpn_ds, predicted, pcd_path))
+    plt.plot(np.linspace(0.0, 0.1), miou_curve)
+    plt.title("mIoU Curve")
+    plt.xlabel("Distance Threshold")
+    plt.ylabel("mIoU")
+    plt.grid(True)
+    plt.savefig(out_root / "mIoU.png")
+    return miou_curve[-1]
 
 # ----------------------------
 # Dataset & Prediction
@@ -187,7 +291,7 @@ def make_loader(opt: argparse.Namespace) -> DataLoader:
             ),
         ]
         )
-        if opt.model == "Ours"
+        if opt.model == "Ours" or opt.model == "Partial"
         else None
     )
 
@@ -226,6 +330,8 @@ def make_loader(opt: argparse.Namespace) -> DataLoader:
         cfg_kwargs["mesh_dir"] = "/mnt/slow/shapenetcorev2-source/"
         cfg_kwargs["points_dir"] = "/mnt/slow/shapenetcorev2-source/"
         cfg_kwargs["split_file"] = "./data/shapenet_split/splits_out.csv"
+        cfg_kwargs["n_partial_samples"] = 2
+        # cfg_kwargs["phase"] = "test"
         # cfg_kwargs["ckpt_dir"] = "logs/original-50k-iters/"
         # cfg_kwargs["db"] = "airplane-10kpt-50k-iters-train-results.db"
         # cfg_kwargs["db_root"] = "db/train/original-50k-iters/"
@@ -241,13 +347,13 @@ def make_loader(opt: argparse.Namespace) -> DataLoader:
         batch_size=opt.batch_size,
         shuffle=False,
         num_workers=opt.num_workers,
-        collate_fn=collate_fn if opt.model == "Ours" else None,
+        collate_fn=collate_fn if (opt.model == "Ours") or (opt.model == "Partial") else None,
         drop_last=False,
     )
 
 def run_reconstruction(model, loader, opt=None, save=True, out_dir=Path("output")):
     cds, emds = [], []
-    recons, gts, inputs, names = [], [], [], []
+    recons, gts, inputs, names, patsam_names = [], [], [], [], []
     kps = []
 
     model.model.eval()
@@ -261,16 +367,60 @@ def run_reconstruction(model, loader, opt=None, save=True, out_dir=Path("output"
                 key = "partial_orig"
             
             if opt.model == "Ours" or opt.model == "Partial":
-                recon_batch, input_pc, full_pc = model.get_reconstruction(batch, key=key)
+                recon_batch, input_pc, full_pc, kp_batch = model.get_reconstruction(batch, key=key)
             else:
                 raise RuntimeError("undefined model case")
-                
+            
             input_batch = input_pc.float()
             gt_batch = full_pc.float()
-            name_batch = batch["target_name"]
+            name_batch = batch["target_file"]
+            patsam_batch = batch["target_partial_sample_name"]
             B = gt_batch.shape[0]
 
             recon_batch = recon_batch.squeeze()    # [B, 1, 2048, 3] to [B, 2048, 3]
+
+            # ---------------- load gt keypoints ----------------
+            # load the kp annotation file
+            annotation_json = Path(f"/mnt/slow/shapenetcorev2-h5/annotations/{batch['target_cat'][0]}.json")
+            # build up input list of dicts
+            kpn_ds = json.load(open(annotation_json))
+            kpn_ds_batch = []
+            mids = batch['target_model_id']
+            cids = batch['target_category']
+            
+            model_found = [False]*B
+            for idx, (cid, mid) in enumerate(zip(cids, mids)):
+                for entry in kpn_ds:
+                    if entry["model_id"] == mid and entry["class_id"] == cid:
+                        kpn_ds_batch.append(entry)
+                        model_found[idx] = True
+                        break
+            
+            # calculate the metrics for the batch
+            nfact_batch = [[maxP, minP] for maxP, minP in zip(batch["nfact_max"], batch["nfact_min"])]
+            fwd_batch = fwd_alignment_scores(kpn_ds_batch, {"kpcd": kp_batch.cpu().numpy(), "nfact": nfact_batch})
+            
+            # convert the format of the gt kp
+
+            
+            # ---------------- load gt keypoints ----------------
+
+
+            # ---------------- hacky filter for the recon ----------------
+            dist_thresh = 3
+            mean_pt = torch.mean(recon_batch.cpu().numpy(), 1).unsqueeze(1)  # [B, 1, 3]
+            dist_from_mean = np.linalg.norm(recon_batch - mean_pt, axis=2)
+            in_mask = dist_from_mean < dist_thresh
+            while not np.all(in_mask):
+                recon_batch = recon_batch.cpu().numpy()[in_mask]
+                mean_pt = torch.mean(recon_batch, 1).unsqueeze(1)  # [B, 1, 3]
+                dist_from_mean = np.linalg.norm(recon_batch - mean_pt, axis=2)
+                in_mask = dist_from_mean < dist_thresh
+                
+            # make sure recon_batch still has the same number of points as gt_batch
+            n_pts = recon_batch.shape[1]
+            gt_batch = gt_batch[:, :n_pts, :]
+            # -----------------------------------------------------------
 
             # One call for all candidates
             res = EMD_CD_recon(recon_batch, gt_batch, reduced=False)
@@ -283,16 +433,10 @@ def run_reconstruction(model, loader, opt=None, save=True, out_dir=Path("output"
             gts.extend(list(gt_batch.cpu().numpy()))
             inputs.extend(list(input_batch.cpu().numpy()))
             names.extend(name_batch)
-            
-            if opt.save_kps:
-                data_dict = model.get_network_data(batch, key=key)
-                kp_batch = model.get_keypoints(data_dict)
-                kps.extend(list(kp_batch))
+            patsam_names.extend(patsam_batch)
+            kps.extend(list(kp_batch.cpu().numpy()))
 
-    save_recon_geoms(recons, gts, inputs, names, cds, emds, opt, out_dir=out_dir)
-    if opt.save_kps:
-        # print("saving prediction keypoints")
-        save_pred_kp_geoms(kps, names, opt, out_dir=out_dir, vis_max=1000)
+    save_recon_geoms(recons, gts, inputs, names, patsam_names, kps, cds, emds, opt, out_dir=out_dir)
 
     cd_mean = np.mean(cds)
     emd_mean = np.mean(emds)
